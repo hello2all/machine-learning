305e90938c53116dc674e3c6de4f3b70799cee4c
hello2all <hellot2@hotmail.com>
2016-11-07 21:25:31 +0800

    update to the newest version of smartcab complete most of the ipython questions unable to improve test results

--- projects/smartcab/smartcab/agent.py
+++ projects/smartcab/smartcab/agent.py
@@ -15,10 +15,26 @@ class LearningAgent(Agent):
         # Initialize any additional variables here
         self.actions = valid_actions = [None, 'forward', 'left', 'right']
         self.state = None
-        self.Qlearner = Qlearn(self.actions)
-    def reset(self, destination=None):
+        self.Qlearner = Qlearn(self.actions) # train
+        # test
+        # self.Qlearner = Qlearn(self.actions, epsilon=0.0)
+        # self.Qlearner.q = pickle.load(open("q.pkl"))
+    def reset(self, destination=None, testing=False):
+        """ The reset function is called at the beginning of each trial.
+            'testing' is set to True if testing trials are being used
+            once training trials have completed. """
+
+        # Select the destination as the new location to route to
         self.planner.route_to(destination)
-        # TODO: Prepare for a new trip; reset any variables here, if required
+        
+        ########### 
+        ## TO DO ##
+        ###########
+        # Update epsilon using a decay function of your choice
+        # Update additional class parameters as needed
+        # If 'testing' is True, set epsilon and alpha to 0
+
+        return None
 
     def vectorize(self, value): # return a index for possible values, 0,1,2,4 for 'None', 'forward', 'left', 'right' respectively and 0,1 for 'green', 'red'respectively
         if value == None or value == 'green':
@@ -30,7 +46,7 @@ class LearningAgent(Agent):
         if value == 'right':
             return '3'
     def sense(self, inputs, next_waypoint):
-        state_string  = ''
+
         self.state = self.vectorize(inputs['light']) + self.vectorize(inputs['oncoming']) + self.vectorize(inputs['right']) + self.vectorize(inputs['left']) + self.vectorize(next_waypoint)
 
     def update(self, t):
@@ -61,21 +77,47 @@ class LearningAgent(Agent):
         # print self.state
 
 def run():
-    """Run the agent for a finite number of trials."""
+    """ Driving function for running the simulation. 
+        Press ESC to close the simulation, or [SPACE] to pause the simulation. """
 
-    # Set up environment and agent
-    e = Environment()  # create environment (also adds some dummy traffic)
-    a = e.create_agent(LearningAgent)  # create agent
-    e.set_primary_agent(a, enforce_deadline=False)  # specify agent to track
-    # NOTE: You can set enforce_deadline=False while debugging to allow longer trials
+    ##############
+    # Create the environment
+    # Flags:
+    #   verbose     - set to True to display additional output from the simulation
+    #   num_dummies - discrete number of dummy agents in the environment, default is 100
+    #   grid_size   - discrete number of intersections (columns, rows), default is (8, 6)
+    env = Environment()
+    
+    ##############
+    # Create the driving agent
+    # Flags:
+    #   learning   - set to True to force the driving agent to use Q-learning
+    #    * epsilon - continuous value for the exploration factor, default is 1
+    #    * alpha   - continuous value for the learning rate, default is 0.5
+    agent = env.create_agent(LearningAgent)
+    
+    ##############
+    # Follow the driving agent
+    # Flags:
+    #   enforce_deadline - set to True to enforce a deadline metric
+    env.set_primary_agent(agent)
 
-    # Now simulate it
-    sim = Simulator(e, update_delay=0, display=False)  # create simulator (uses pygame when display=True, if available)
-    # NOTE: To speed up simulation, reduce update_delay and/or set display=False
+    ##############
+    # Create the simulation
+    # Flags:
+    #   update_delay - continuous time (in seconds) between actions, default is 2.0 seconds
+    #   display      - set to False to disable the GUI if PyGame is enabled
+    #   log_metrics  - set to True to log trial and simulation results to /logs
+    #   optimized    - set to True to change the default log file name
+    sim = Simulator(env)
+    
+    ##############
+    # Run the simulator
+    # Flags:
+    #   tolerance  - epsilon tolerance before beginning testing, default is 0.05 
+    #   n_test     - discrete number of testing trials to perform, default is 0
+    sim.run()
 
-    sim.run(n_trials=10000)  # run for a specified number of trials
-    # NOTE: To quit midway, press Esc or close pygame window, or hit Ctrl+C on the command-line
-    pickle.dump(a.Qlearner.q, open('q.pkl','wb'))
 
 if __name__ == '__main__':
     run()
